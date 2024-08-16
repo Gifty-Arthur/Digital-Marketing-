0ff33f39e02129ea0fde8df1a7bf4560fcf5992a
Gifty Arthur <giftyarthur031@gmail.com>
2024-08-16 16:24:48 +0000

    update

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 const Hero = () => {
   return (
@@ -12,22 +15,31 @@ const Hero = () => {
         className="absolute inset-0"
       />
       <div className="relative z-10 container py-20">
-        <h1 className="text-primary text-4xl roboto font-bold">
-          Boost{" "}
-          <span className="text-white">
-            Your brand with <br />
-            Innovative{" "}
-            <span className="text-primary">
-              Digital
-              <br />
-              Marketing
-            </span>
-          </span>{" "}
-        </h1>
-        <p className="text-white roboto text-md mt-4">
-          Drive Engagement and Conversions with Advanced Digital <br />
-          Marketing Strategies.
-        </p>
+        <div className="flex justify-between items-center">
+          <div className="">
+            <h1 className="text-primary text-4xl roboto font-bold">
+              Boost{" "}
+              <span className="text-white">
+                Your brand with <br />
+                Innovative{" "}
+                <span className="text-primary">
+                  Digital
+                  <br />
+                  Marketing
+                </span>
+              </span>{" "}
+            </h1>
+            <p className="text-white roboto text-md mt-4">
+              Drive Engagement and Conversions with Advanced Digital <br />
+              Marketing Strategies.
+            </p>
+            <Link href="/">
+              <button className="">Start Now</button>
+            </Link>
+          </div>
+
+          <Image src="/images/home.png" alt="home" width={500} height={500} />
+        </div>
       </div>
     </div>
   );
